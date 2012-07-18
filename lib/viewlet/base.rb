c7696d0240f4dd7b9db8f65931206cb77e3ad61e
Dmitriy Kalinin <cppforlife@gmail.com>
2012-07-18 01:42:31 -0700

    expose viewlet instance in its plugin.html.haml

--- lib/viewlet/base.rb
+++ lib/viewlet/base.rb
@@ -4,23 +4,44 @@ module Viewlet
       @name = name
       @view = view
       @variables = {
+        @name.to_sym => self,
         :unique_id => "viewlet_#{rand(36**20).to_s(36)}"
       }
     end
 
+    def render
+      file_path = Rails.root.join("app/viewlets/#{@name}/plugin").to_s
+      @view.render(:file => file_path, :locals => @variables)
+    end
+
+    private
+
     def method_missing(method, *args, &block)
-      @variables[method] = if block_given?
+      is_write_op = if @variables[method].is_a?(Proc)
+        block.present?
+      else
+        args.any? || block.present?
+      end
+
+      send("_#{is_write_op ? :write : :read}_variable", method, *args, &block)
+    end
+
+    def _read_variable(name, *args, &block)
+      if @variables[name].is_a?(Proc)
+        @view.capture(*args, &@variables[name])
+      else
+        @variables[name]
+      end
+    end
+
+    def _write_variable(name, *args, &block)
+      @variables[name] = if block
         # HAML changes argument-less block {|| } into block {|*a| }
         # which makes block.arity to be -1 instead of just 0
         block.arity == -1 ? @view.capture(&block) : block
       else
         args.first
       end
-    end
-
-    def render
-      file_path = Rails.root.join("app/viewlets/#{@name}/plugin").to_s
-      @view.render(:file => file_path, :locals => @variables)
     end
   end
 end
